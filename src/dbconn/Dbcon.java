f99fe49d35bb386705445a896715e2b9f8eff8c0
107598045 <seraph0690@gmail.com>
2019-10-21 12:02:36 +0800

    修改dbconn

--- src/dbconn/Dbcon.java
+++ src/dbconn/Dbcon.java
@@ -1,4 +1,4 @@
-﻿package dbconn;
+package dbconn;
 
 import com.mysql.cj.jdbc.Driver;
 import java.sql.Connection;
@@ -6,16 +6,16 @@ import java.sql.DriverManager;
 
 public class Dbcon {
     public final static String SACMS = "sacms";
-    public final static String SACMS_CACHE = "sacms_cache";
+    private final static String userName = "root";
+    private final static String password = "password";
 
     public static Connection getConnection(String dbName) {
-//不能看
+        return getConnection(dbName, userName, password);
     }
 
     public static Connection getConnection(String dbName, String userName, String password) {
         try {
-            String url = "jdbc:mysql://" + dbName + "?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC&useSSL=false";
-//            String url = "jdbc:mysql://" + dbName + "?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC&useSSL=false";  //phpmyadmin
+            String url = "jdbc:mysql://localhost:3306/" + dbName + "?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC&useSSL=false";
             DriverManager.registerDriver(new Driver());
             return DriverManager.getConnection(url, userName, password);
         }catch (Exception e) {
